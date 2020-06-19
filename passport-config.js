32662579104a69f097d3bd28ef53af90e35b9dd5
bagastri07 <bagaskuhaku@gmail.com>
2020-06-19 08:40:22 +0700

    FINISH

---
+++ passport-config.js
@@ -0,0 +1,30 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+function initialize(passport, getUserByEmail, getUserById) {
+    const authenticatUser = async (email, password, done) => {
+        const user = getUserByEmail(email)
+        if (user == null) {
+            return done(null, false, { message: 'No user with that email'})
+        }
+
+        try {
+            if(await bcrypt.compare(password, user.password)) {
+                return done(null, user)
+            } else {
+                return done(null, false, { message: "Password incorrect"})
+            }
+        } catch {
+            return done(e)
+        }
+    }
+
+    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticatUser))
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id))
+    })
+
+}
+
+module.exports = initialize
