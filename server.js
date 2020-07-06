209d7611a39b041cc1fc02d34f1aaf46612bd9e4
bagastri07 <bagaskuhaku@gmail.com>
2020-07-07 00:33:42 +0700

    update

--- server.js
+++ server.js
@@ -9,15 +9,23 @@ const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
+const db = require('./conn')
+const request = require('request')
 
 const initializePassport = require('./passport-config')
+const { json } = require('express')
 initializePassport(
     passport,
     email => users.find(user => user.email === email),
     id => users.find(user => user.id === id)
 )
 
-const users = []
+var users = [];
+
+function setValue(value) {
+    users = value;
+    console.log(users);
+}
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded( { extended: false }))
@@ -37,6 +45,14 @@ app.get('/', checkAuthenticated, (req, res) => {
 
 app.get('/login', checkNotAuthencticated, (req, res) => {
     res.render('login.ejs')
+    db.query('SELECT * FROM tb_user', (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            //console.log(result)
+            setValue(JSON.parse(JSON.stringify(result)));
+        }
+    })
 })
 
 app.post('/login', checkNotAuthencticated, passport.authenticate('local', {
@@ -50,13 +66,18 @@ app.get('/register', checkNotAuthencticated,  (req, res) => {
 })
 
 app.post('/register', checkNotAuthencticated, async (req, res) => {
-        try {
+    try {
         const hashPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
+        let data = {
             name: req.body.name,
             email: req.body.email,
             password: hashPassword
+        }
+        let queryString = 'INSERT INTO tb_user SET ?'
+        db.query(queryString, data, (err, result) => {
+            if (err){
+                res.send({msg: err})
+            }
         })
         res.redirect('/login')
     } catch {
@@ -83,4 +104,6 @@ function checkNotAuthencticated(req, res, next) {
     next()
 }
 
-app.listen(3003)
+app.listen(3003, () => {
+    console.log('Listening at 3003')
+})
